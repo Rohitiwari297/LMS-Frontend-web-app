71d21358c0adabcb5a6568ea21c33c81b380647f
Rohitiwari <tripathirohit297@gmail.com>
2025-06-03 16:21:40 +0530

    configured auto import sorting for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
   { ignores: ['dist'] },
@@ -19,11 +20,13 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': eslintPluginSimpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'simple-import-sort/imports': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
