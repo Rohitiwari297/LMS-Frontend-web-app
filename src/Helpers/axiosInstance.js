2f457b134404775d30ed4b664e1189cf3c5c1541
Rohitiwari <tripathirohit297@gmail.com>
2025-06-04 11:39:13 +0530

    set of axios instance done

---
+++ src/Helpers/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { AiFillAccountBook } from "react-icons/ai";
+
+const BASE_URL = "http://localhost:5014/api/v1";
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true
+
+export default axiosInstance;
